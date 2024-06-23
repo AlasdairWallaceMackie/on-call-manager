60e13f4ba1de471793f21a6fd4466a6c36ce5a3f
AlasdairWallaceMackie <73504675+AlasdairWallaceMackie@users.noreply.github.com>
2024-06-23 14:32:20 -0400

    Basic components, add bootstrap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  // List of Google accounts that are permitted to log in
+  let users = [
+    'amanager@company.com',
+    'bmanager@company.com',
+  ]
+
+  let employeeNames = [
+    "Tuomas Holopainen",
+    "Emppu Vuorinen",
+    "Troy Donockley",
+    "Floor Jansen",
+    "Kai Hahto",
+    "Jukka Koskinen",
+  ]
+</script>
+
+
+<main>
+  <h1>On Call Manager</h1>
+
+  {#each employeeNames as name}
+    <div>
+      {name}
+    </div>
+  {/each}
+
+  <button>Submit Changes</button>
+
+  <fieldset>
+    <legend>Current Status</legend>
+
+    All Ok
+  </fieldset>
+
+  <input class="form-control" type="text" />
+</main>
